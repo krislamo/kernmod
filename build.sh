e48f1aaf3cc4c0c604604070c455f1fe22d70ded
Kris Lamoureux <kris@lamoureux.io>
2022-01-31 23:04:50 -0500

    Build and install DKMS Debian package

--- build.sh
+++ build.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
+PACKAGE="helloworld"
 VERSION="0.1"
+REVISION="1"
+BUILDDIR="$(mktemp -d)/${PACKAGE}_$VERSION-$REVISION"
 
-# Install build prerequisites
+# Place sources and build package
+mkdir -p "$BUILDDIR/usr/src/${PACKAGE}-$VERSION"
+mkdir -p "$BUILDDIR/etc"
+mkdir -p "$BUILDDIR/DEBIAN"
+cp -r /vagrant/src/usr/src/* "$BUILDDIR/usr/src/${PACKAGE}-$VERSION"
+cp -r /vagrant/src/etc/* "$BUILDDIR/etc"
+cp -r /vagrant/src/DEBIAN/* "$BUILDDIR/DEBIAN"
+cd "$BUILDDIR/.."
+dpkg-deb --build "${PACKAGE}_$VERSION-$REVISION"
+
+# Install package
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y dkms build-essential linux-headers-$(uname -r)
-
-# Place source and configuration files on the system
-cd /vagrant
-mkdir /usr/src/helloworld-$VERSION
-cp Makefile /usr/src/helloworld-$VERSION/Makefile
-cp helloworld.c /usr/src/helloworld-$VERSION/helloworld.c
-cp dkms.conf /usr/src/helloworld-$VERSION/dkms.conf
-cp helloworld.conf /etc/modules-load.d/helloworld.conf
-
-# Build and install kernel module with DKMS
-dkms add -m helloworld -v $VERSION
-dkms build -m helloworld -v $VERSION
-dkms install -m helloworld -v $VERSION
+apt-get install -y linux-headers-$(uname -r)
+apt-get install -y "./${PACKAGE}_$VERSION-$REVISION.deb"
 
 # Load module and show details about it
-modprobe helloworld
 modinfo helloworld
 cat /proc/modules | grep helloworld
 rmmod helloworld
+modprobe helloworld
 cat /var/log/messages | grep helloworld
